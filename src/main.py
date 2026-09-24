bc3c8599df6a163c87e1acfc00db93a148312746
benbernut-school <26012742@saultcollege.ca>
2026-09-24 21:04:11 +0000

    Lab 1 done

---
+++ src/main.py
@@ -0,0 +1,20 @@
+print("Please enter your NAME")
+name = input()
+
+print("Please create your USERNAME:")
+username = input()
+
+print("Please create your PASSWORD")
+password = input()
+
+login = username + password
+
+print("Please enter your USERNAME")
+while input() != username:
+    print("Incorrect try again!")
+print("Correct!")
+
+print("Please enter your PASSWORD")
+while input() != password:
+    print("Incorrect try again!")
+print("Correct!")
